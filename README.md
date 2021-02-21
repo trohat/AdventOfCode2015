5826454ae804d0e3932cb93bce58a003157b0857
Trohat <petr.sed@centrum.cz>
2021-02-21 12:45:58 +0100

    Template, tests, helpers & README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2015
+
+Codes from [Christmas coding event](https://adventofcode.com/2015). 
+
+#### Useful functionalities: 
+
+... nothing yet ...
---
+++ template/index.js
@@ -0,0 +1,39 @@
+console.log("AOC 2015 - ");
+
+const splitLines = data => data.split(String.fromCharCode(10));
+
+const prepare = data => data;
+
+const task1 = data => {
+    
+};
+
+const task2 = data => {
+    
+}
+
+let testdata = ``;
+
+inputdata = prepare(splitLines(inputdata));
+
+console.log(inputdata);
+
+testdata = prepare(splitLines(testdata));
+
+console.log(testdata);
+
+console.log("");
+
+//doEqualTest(task1(testdata), 7);
+
+//console.time("Task 1");
+//console.log("Task 1: " + task1(inputdata));
+//console.timeEnd("Task 1");
+
+console.log("");
+
+//doEqualTest(task2(testdata), 336);
+
+//console.time("Task 2");
+//console.log("Task 2: " + task2(inputdata));
+//console.timeEnd("Task 2");
